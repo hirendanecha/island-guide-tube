bd546a5f18053355b55a82330e26b22678052fbc
hirendanecha <hirendanecha@gmail.com>
2024-08-20 16:46:59 +0530

    initial commit

---
+++ src/app/main-layout/main-layout.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ShareService } from '../@shared/services/share.service';
+import { BreakpointService } from '../@shared/services/breakpoint.service';
+import { environment } from 'src/environments/environment';
+import { CommonService } from '../@shared/services/common.service';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from '../@shared/services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-main-layout',
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss'],
+})
+export class MainLayoutComponent implements OnInit {
+  tokenData: any;
+  constructor(
+    public shareService: ShareService,
+    public breakpointService: BreakpointService,
+    private commonService: CommonService,
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private authService: AuthService,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit(): void {
+    this.spinner.show();
+    const url = environment.apiUrl + 'login/me';
+    this.commonService
+      .get(url, {
+        withCredentials: true,
+      })
+      .subscribe({
+        next: (res) => {
+          this.spinner.hide();
+          this.tokenData = { ...res };
+          const auth = this.tokenData?.user;
+          const token = this.tokenData?.accessToken;
+          const isLogin = token && auth ? true : false;
+          this.authService.setToken(token);
+          this.shareService.getUserDetails(auth?.profileId);
+          this.authService.userDetails = auth;
+          this.authService.token = token;
+          // if (!isLogin) {
+          //   location.href = environment?.loginUrl;
+          // }
+        },
+        error: (err) => {
+          this.spinner.hide();
+          this.cookieService.delete('auth-user', '/', environment.domain);
+          const url = environment.apiUrl + 'customers/logout';
+          this.commonService.get(url).subscribe({
+            next: (res) => {
+              localStorage.clear();
+              sessionStorage.clear();
+              location.href = environment.logoutUrl;
+            },
+          });
+          console.log(err);
+        },
+      });
+  }
+}
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,25 @@
+// const frontendUrl = 'http://localhost:4200/';
+// const backendUrl = 'http://localhost:8080/';
+// const loginUrl = 'http://localhost:4200/login';
+
+const frontendUrl = 'https://tube.islandguide.tours/';
+const backendUrl = 'https://api.islandguide.tours/';
+const loginUrl = 'https://dev.islandguide.tours/login';
+const wasabiUrl = 'https://freedom-social.s3.us-east-1.wasabisys.com/';
+const logoutUrl = 'https://dev.islandguide.tours/logout';
+
+
+
+export const environment = {
+  production: true,
+  frontendUrl: frontendUrl,
+  backendUrl: backendUrl,
+  loginUrl: loginUrl,
+  apiUrl: `${backendUrl}api/v1/`,
+  domain: '.islandguide.tours',
+  wasabiUrl: wasabiUrl,
+  socketUrl: `${backendUrl}`,
+  conferenceUrl: 'https://meet.facetime.tube/',
+  logoutUrl: logoutUrl
+};
+
